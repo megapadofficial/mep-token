b76e8f1da6febc0441758c006e21c9a0479c5f48
mep-dev04 <86368153+mepdev04@users.noreply.github.com>
2022-01-28 17:33:59 +0700

    Update MegapadToken.sol

--- contracts/MegapadToken.sol
+++ contracts/MegapadToken.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.11;
+pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
